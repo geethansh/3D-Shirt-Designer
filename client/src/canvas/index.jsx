0ee5de934f31be9762cb1ea92efee805290768d0
Geethansh <geethansh@gmail.com>
2024-02-14 20:38:52 +0530

    first commit

---
+++ client/src/canvas/index.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import {Canvas} from '@react-three/fiber'
+import {Environment, Center} from '@react-three/drei'
+import Shirt from './Shirt'
+import Backdrop from './Backdrop'
+import Camerarig from './Camerarig'
+const CanvasModel = () => {
+  return (
+    <Canvas>
+      <ambientLight intensity={0.5}/>
+      <Environment preset='city'/>
+      <Camerarig>
+        {/* <Backdrop/> */}
+        <Center>
+          <Shirt/>
+        </Center>
+      </Camerarig>
+
+    </Canvas>
+  )
+}
+
+export default CanvasModel
